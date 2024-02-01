b1d88c41bd047038a8bfabe49c704229881d09ce
unknown <buikimquangloc@gmail.com>
2024-02-01 12:22:14 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const ApiError = require("./app/api-error");
 
 const contactsRouter = require("./app/routes/contact.route");
 
@@ -8,8 +9,19 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 
+app.use((err, req, res, next) => {
+    return res.status(console.error.statusCode || 500).json ({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to my app!" });
 });
+
 
 module.exports = app;
